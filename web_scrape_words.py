2d8767c8d81caee967eee01e3d6f07a11f1c4fbe
nmathias2101@gmail.com <nmathias2101@gmail.com>
2023-03-18 19:17:00 -0700

    create get_words function

--- web_scrape_words.py
+++ web_scrape_words.py
@@ -8,35 +8,46 @@ from bs4 import BeautifulSoup
 
 # list of pages containing words with different themes
 # link : https://www.enchantedlearning.com/wordlist/
-words_page_names = ["astronomy", "positivewords", "languages", "metals", "usstates", "vegetables"]
-synonym_words_page_names = ["big", "happy", "said"]
-words_dict = {}
 
-# make directory to store words in files if does not exist already
-if not os.path.isdir('words/') :
-    os.mkdir('words/')
-else:
-    shutil.rmtree('words/')
-    time.sleep(3)           #time to delete files in dir & the dir itsellf
-    os.mkdir('words/')
+def get_words() :
+    '''
+    web scraping to get words & store in separate files based on theme
 
-for page_name in words_page_names + synonym_words_page_names :
-    # get URL request
-    URL = "https://www.enchantedlearning.com/wordlist/" + page_name + ".shtml"
-    page = requests.get(URL)
+    returns a dictionary where
+        keys : themes
+        values : list of words
+    '''
+    words_page_names = ["astronomy", "positivewords", "languages", "metals", "usstates", "vegetables"]
+    synonym_words_page_names = ["big", "happy", "said"]
+    words_dict = {}
 
-    # get page content in html format
-    # aim is to get class names & type of tags
-    soup = BeautifulSoup(page.content, "html.parser")
+    # make directory to store words in files if does not exist already
+    if not os.path.isdir('words/') :
+        os.mkdir('words/')
+    else:
+        shutil.rmtree('words/')
+        time.sleep(3)           #time to delete files in dir & the dir itsellf
+        os.mkdir('words/')
 
-    words_list = []
-    for sp in soup.find_all("div",class_="wordlist-item"):
-        words_list.append(sp.text.lower())              #lower text for game purposes
+    for page_name in words_page_names + synonym_words_page_names :
+        # get URL request
+        URL = "https://www.enchantedlearning.com/wordlist/" + page_name + ".shtml"
+        page = requests.get(URL)
 
-    words_dict[page_name] = words_list
+        # get page content in html format
+        # aim is to get class names & type of tags
+        soup = BeautifulSoup(page.content, "html.parser")
 
-    # write all words with same theme in separate file
-    with open('words/' + page_name + '.txt', "w") as file:
-        for word in words_list:
-            file.write(word + '\n')
+        words_list = []
+        for sp in soup.find_all("div",class_="wordlist-item"):
+            words_list.append(sp.text.lower())              #lower text for game purposes
+
+        words_dict[page_name] = words_list
+
+        # write all words with same theme in separate file
+        with open('words/' + page_name + '.txt', "w") as file:
+            for word in words_list:
+                file.write(word + '\n')
+
+    return words_dict
 
